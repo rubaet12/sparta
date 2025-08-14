f6f22f7831e15f87d8985a31e4fa8c8b9855276f
Rubaet Mohammed <mohammedrubaet07440@gmail.com>
2025-08-14 11:02:26 +0100

    scripts

--- app/prov-db.sh
+++ app/prov-db.sh
@@ -1,131 +1,61 @@
 #!/bin/bash
-
-
-
-
-
-
-
-
-echo 
-
-echo " Updating System Packages"
-
-echo 
-
-sudo DEBIAN_FRONTEND=noninteractive apt-get update -y
-
-sudo DEBIAN_FRONTEND=noninteractive apt-get upgrade -y
-
-echo "System update complete."
-
+ 
+#Scipt for the database
+ 
+# Update the package list to make sure the latest versions are installed
+echo "update..."
+sudo DEBIAN_FRONTEND=noninteractive apt-get update
+echo "update done"
 echo
-
-
-
-
-
-echo 
-
-echo " Importing MongoDB 7.0 Public GPG Key"
-
-echo 
-
+ 
+echo "upgrade..."
+sudo DEBIAN_FRONTEND=noninteractive apt upgrade -y
+echo "upgrade done"
+echo
+ 
+sudo DEBIAN_FRONTEND=noninteractive apt-get install gnupg curl
+ 
+echo "import public key"
 curl -fsSL https://www.mongodb.org/static/pgp/server-7.0.asc | \
-
 gpg --dearmor | sudo tee /usr/share/keyrings/mongodb-server-7.0.gpg > /dev/null
-
-echo "GPG key added."
-
+echo "imported public key"
 echo
-
-
-
-echo
-echo " Creating MongoDB APT Source List File"
-
-echo
-echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-7.0.gpg ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/7.0 multiverse" | \
-
-    sudo tee /etc/apt/sources.list.d/mongodb-org-7.0.list > /dev/null
-
-
-
-echo "Reloading package list..."
-
-sudo DEBIAN_FRONTEND=noninteractive apt-get update -y
-
-echo
-
-
-
-echo
-echo " Installing MongoDB 7.0.22"
-
-echo 
+ 
+#create list file
+echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-7.0.gpg ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/7.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-7.0.list
+ 
+sudo DEBIAN_FRONTEND=noninteractive apt-get update
+ 
+#install mongodb
 sudo DEBIAN_FRONTEND=noninteractive apt-get install -y \
-
    mongodb-org=7.0.22 \
-
    mongodb-org-database=7.0.22 \
-
    mongodb-org-server=7.0.22 \
-
    mongodb-mongosh \
-
    mongodb-org-shell=7.0.22 \
-
    mongodb-org-mongos=7.0.22 \
-
    mongodb-org-tools=7.0.22 \
-
    mongodb-org-database-tools-extra=7.0.22
-
-echo "MongoDB installed."
-
-echo
-
-
-
-echo 
-
+ 
+   echo
 echo " Configuring MongoDB to Allow External Connections"
-
-echo 
-
-
-
-# Backup existing config
-
-sudo cp /etc/mongod.conf /etc/mongod.conf.bk
-
-
-
-# Update bindIp to 0.0.0.0
-
-sudo sed -i 's/bindIp: 127.0.0.1/bindIp: 0.0.0.0/' /etc/mongod.conf
-
-echo "bindIp updated to 0.0.0.0"
-
 echo
-
+ 
+# Backup existing config
+sudo cp /etc/mongod.conf /etc/mongod.conf.bk
+ 
+# Update bindIp to 0.0.0.0
+sudo sed -i 's/bindIp: 127.0.0.1/bindIp: 0.0.0.0/' /etc/mongod.conf
+echo "bindIp updated to 0.0.0.0"
+echo
+ 
 echo
 echo " Starting MongoDB..."
-
-echo 
-
+echo
 sudo systemctl start mongod
-
 sudo systemctl enable mongod
-
-
-
-
-
+ 
 echo
-
 echo
-
 echo " MongoDB provisioned and running "
-
 echo
